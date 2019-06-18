f5f1e214ebcf73f656709791c9a18597a0a0360e
flege <abd.alfaruq@gmail.com>
2019-06-18 15:52:45 +0700

    create comment

--- app/src/main/java/com/flege/buku/MainActivity.java
+++ app/src/main/java/com/flege/buku/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //testing
         btIns = findViewById(R.id.btIns);
         btIns.setOnClickListener(new View.OnClickListener() {
             @Override
